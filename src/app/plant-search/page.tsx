14f7dee4581ff1a1fdd5b0643ba33f64b4af0ed8
Omar Mohammed Elsayed Elbedawy <omar.1824039@stemksheikh.moe.edu.eg>
2025-08-20 19:57:42 +0000

    Skip to content New Project – Vercel
    Find…
    F
    
    New Project
    importing fro

--- src/app/plant-search/page.tsx
+++ src/app/plant-search/page.tsx
@@ -1,12 +1,19 @@
 
 import { PlantForm } from '@/components/plant-form';
+import { Suspense } from 'react';
+
+function PlantSearchFallback() {
+    return <></>
+}
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-background text-foreground font-body flex items-center justify-center">
       <main className="container mx-auto p-4 py-8 md:p-12">
         <div className="w-full max-w-md mx-auto">
-          <PlantForm />
+            <Suspense fallback={<PlantSearchFallback/>}>
+                <PlantForm />
+            </Suspense>
         </div>
       </main>
     </div>
